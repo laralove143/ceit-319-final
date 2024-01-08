46fce6fcbe57e09f61b2cd6ed8bc0451fce024c8
laralove <laralove143@icloud.com>
2024-01-08 20:38:58 +0300

    add reflection and footer

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function setHeight() {
-  let gridContainer = document.getElementById("gridContainer");
-  gridContainer.style.height = `calc(${window.innerHeight}px - 5vh)`;
+  document.body.style.height = `${window.innerHeight}px`;
 }
 
 setHeight();
